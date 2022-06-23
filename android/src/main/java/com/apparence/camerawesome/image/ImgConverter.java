e312f4bf08650ef6741096602957f0a4bee71dda
SERGIO RICARDO RAMOS <sramos30@hotmail.com>
2022-06-23 08:05:39 -0300

    example changes

--- android/src/main/java/com/apparence/camerawesome/image/ImgConverter.java
+++
@@ -1,9 +0,0 @@
-package com.apparence.camerawesome.image;
-
-import android.media.Image;
-import android.media.ImageReader;
-
-public interface ImgConverter {
-
-    byte[] process(ImageReader imageReader);
-}
